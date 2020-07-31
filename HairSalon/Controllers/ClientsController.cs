4657e2c43060ee0be2065485ac8f02b3ea71988f
Kate Skorija <kateskorija@gmail.com>
2020-07-31 11:48:47 -0700

    create index route and view for clients controller, update links in stylists views

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      db = _db;
+    }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+  }
+}
