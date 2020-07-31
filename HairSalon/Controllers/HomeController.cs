0c8c0e0c43a08f691f809fb842ebffb6d642bd56
Kate Skorija <kateskorija@gmail.com>
2020-07-31 10:39:59 -0700

    create index route and view for stylist controller, update home controller

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using RestaurantFinder.Models;
+using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace HairSalon.Controllers
 {
-  public class HomeController : Controllers
+  public class HomeController : Controller
   {
     [HttpGet("/")]
     public ActionResult Index()
