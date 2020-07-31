83cd1b438f593e9570726fc040c617793dbe0230
Kate Skorija <kateskorija@gmail.com>
2020-07-31 09:42:43 -0700

    create stylist class and properties

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -5,6 +5,14 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string WorkStation { get; set; }
+    public int StylistId { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
   }
 }
